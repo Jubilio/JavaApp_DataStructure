976dc9812f57abab7b9cfa5b6ab966ba6b367c14
Jubilio <jubiliomausse@outlook.pt>
2025-06-07 20:43:34 +0200

    feat: NameApp

---
+++ src/App.java
@@ -0,0 +1,56 @@
+
+import javax.swing.JOptionPane;
+
+import service.NameManager;
+
+public class App {
+
+    public static void main(String[] args) {
+        NameManager nameManager = new NameManager();
+
+        int opt = 5;
+
+        do {
+
+            opt = Integer.parseInt(JOptionPane.showInputDialog(
+                    "Choose an option:\n"
+                            + "1. Add Name\n"
+                            + "2. Show Names\n"
+                            + "3. Remove Name\n"
+                            + "4. Clear Names\n"
+                            + "5. Exit"));
+
+            switch (opt) {
+                case 1: {
+                    System.out.println(
+                            nameManager.addName(
+                                    JOptionPane.showInputDialog("Enter a name:")));
+                    break;
+                }
+                case 2:
+                    System.out.println("Showing all names:");
+                    nameManager.showAllNames();
+                    break;
+                case 3: {
+                    System.out.println("Enter the name to remove:");
+                    System.out.println(
+                            nameManager.removeName(
+                                    JOptionPane.showInputDialog("Enter a name:")));
+                    break;
+                }
+                case 4: {
+                    System.out.println("All names cleared.");
+                    nameManager.clearNames();
+                    break;
+                }
+                case 5:
+                    System.out.println("Exiting the application.");
+                    break;
+                default:
+                    System.out.println("Invalid option. Please try again.");
+                    break;
+            }
+
+        } while (opt != 5);
+    }
+}
---
+++ src/service/NameManager.java
@@ -0,0 +1,45 @@
+package service;
+
+import java.util.HashSet;
+
+import javax.swing.JOptionPane;
+
+public class NameManager {
+    private HashSet<String> names;
+
+    public NameManager() {
+        this.names = new HashSet<>();
+    }
+
+    public String addName(final String name) {
+        names.add(name);
+        return "Name: " + name;
+    }
+
+    public void showAllNames() {
+        if (names.isEmpty()) {
+            JOptionPane.showMessageDialog(null, "No names available.");
+
+        }
+        for (String n : names) {
+            // Print to console
+            System.out.println("Name: " + n);
+            JOptionPane.showMessageDialog(null, "Name: " + n);
+        }
+    }
+
+    public String removeName(final String name) {
+        if (names.remove(name)) {
+            return "Name removed: " + name;
+        } else {
+            return "Name not found: " + name;
+        }
+    }
+    /**
+     * Clears all names from the set.
+     */
+    public void clearNames() {
+        names.clear();
+        JOptionPane.showMessageDialog(null, "All names cleared.");
+    }
+}
